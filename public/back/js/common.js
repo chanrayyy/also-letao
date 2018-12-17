193f3ad768a78f23a1ca7a2a72c4a35e1e7f4bdb
ray <892690119qq.com>
2018-12-17 22:36:08 +0800

    index未完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,6 +1,16 @@
+// 进度条
 $(document).ajaxStart(function(){
   NProgress.start();
 })
 $(document).ajaxStop(function(){
   NProgress.done();
+})
+
+//header栏操作
+$(function(){
+  $('.icon_left').click(function(){
+    $('.lt_aside').toggleClass('hidemenu')
+    $('.lt_header').toggleClass('hidemenu')
+    $('.lt_main').toggleClass('hidemenu')
+  })
 })
