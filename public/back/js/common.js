eb38d206634346c6af42fcbec83bf09d377df1b7
ray <892690119qq.com>
2018-12-18 16:13:49 +0800

    index页面完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -6,11 +6,32 @@ $(document).ajaxStop(function(){
   NProgress.done();
 })
 
-//header栏操作
 $(function(){
+  //header栏操作
   $('.icon_left').click(function(){
     $('.lt_aside').toggleClass('hidemenu')
     $('.lt_header').toggleClass('hidemenu')
     $('.lt_main').toggleClass('hidemenu')
+  })
+  $('.icon_right').click(function(){
+    $('#myModal').modal('show')
+  })
+  // aside栏操作
+  $('.cate').click(function(){
+    $('.child').stop().slideToggle()
+  })
+  // 点击退出
+  $('.btn_q').click(function(){
+    $.ajax({
+      type: 'get',
+      url: '/employee/employeeLogout',
+      dataType: 'json',
+      success: function(res){
+        if(res.success){
+          location.href = 'login.html'
+        }
+      }
+
+    })
   })
 })
